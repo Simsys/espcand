751509d938a823d596f00d57a056f1e59b1a7ebf
Simsys <winfried.simon@gmail.com>
2025-07-28 10:26:47 +0200

    Add tch_echo example

---
+++ python/bench_tcp_echo.py
@@ -0,0 +1,28 @@
+import socket
+from datetime import datetime, timedelta
+
+
+HOST = "192.168.178.170"  # The server's hostname or IP address
+PORT = 1234  # The port used by the server
+
+delta = 2
+end_time = datetime.now() + timedelta(seconds=delta)
+received = b""
+expected = b""
+data = b"1234567890"*80
+
+print(f"len(data) {len(data)}")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while datetime.now() < end_time:
+        s.sendall(data)
+        expected += data
+        received += s.recv(1024)
+
+if received != expected:
+    print(f"Error, received Data is not equal sent data")
+    exit(1)
+
+
+print(f"Bytes received {len(received)} bytes -> {(len(received)/delta)//1024} kByte/s")
