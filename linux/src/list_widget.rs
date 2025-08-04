917673fdf8ade4cd23cca66636a0a4151bc07847
Simsys <winfried.simon@gmail.com>
2025-08-04 17:35:09 +0200

    Add tui app for test purposes

---
+++ linux/src/list_widget.rs
@@ -0,0 +1,114 @@
+use ratatui::{
+    Frame,
+    layout::Rect,
+    widgets::{Block, List, ListItem},
+};
+use std::collections::VecDeque;
+use smol::io::{AsyncWrite, Error};
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+#[derive(Debug)]
+pub struct ListWidget<const CAP: usize> {
+    content: VecDeque<String>,
+    border_title: &'static str,
+}
+
+impl<const CAP: usize> ListWidget<CAP> {
+    pub fn new(border_title: &'static str) -> Self {
+        let content = VecDeque::<String>::with_capacity(CAP); 
+        Self { content, border_title }
+    }
+
+    pub fn render(&mut self, frame: &mut Frame, area: &Rect) {
+        let line_count = (area.height - 2) as usize;
+        let start_idx = if self.content.len() > line_count {
+            self.content.len() - line_count
+        } else {
+            0
+        };
+        let content: Vec<ListItem> = self.content
+            .range(start_idx..)
+            .map(|s| ListItem::new(s.clone()))
+            .collect();
+        let content = List::new(content).block(Block::bordered().title(self.border_title));
+        frame.render_widget(content, *area);
+    }
+
+    pub fn add_item(&mut self, item: String) {
+        if self.content.len() == CAP {
+            let _ = self.content.pop_front();
+        }
+        self.content.push_back(item);
+    }
+}
+
+#[derive(Debug)]
+pub struct ListWidgets<const CAP:usize> {
+    can_widget: ListWidget<CAP>,
+    cmd_widget: ListWidget<CAP>,
+}
+
+impl<const CAP:usize> ListWidgets<CAP> {
+    pub fn new() -> Self {
+        Self {
+            can_widget: ListWidget::new(" Received CAN Messages "),
+            cmd_widget: ListWidget::new(" Commands and Messages "),
+        }
+    }
+
+    pub fn can(&mut self) -> &mut ListWidget<CAP> {
+        &mut self.can_widget
+    }
+
+    pub fn cmd(&mut self) -> &mut ListWidget<CAP> {
+        &mut self.cmd_widget
+    }
+
+    pub fn render(&mut self, frame: &mut Frame, can_area: &Rect, cmd_area: &Rect) {
+        self.can_widget.render(frame, can_area);
+        self.cmd_widget.render(frame, cmd_area);
+    }
+}
+
+impl<const CAP:usize> AsyncWrite for &mut ListWidgets<CAP> {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {        
+        let n = buf.len();
+        let mut s = String::new();
+        for c in buf {
+            if *c != b'\n' {
+                if let Some(ch) = char::from_u32(*c as u32) {
+                    s.push(ch);
+                } 
+            } else {
+                if &s.as_str()[..3] == "$RF" {
+                    self.can_widget.add_item(s.clone());
+                } else {
+                    self.cmd_widget.add_item(s.clone());
+                }
+                s.clear();
+            }
+        }
+        Poll::Ready(Ok(n))    
+    }
+    
+    fn poll_flush(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))    
+    }
+
+    fn poll_close(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))    
+    }
+}
--- linux/src/main.rs
+++ linux/src/main.rs
@@ -1,23 +1,123 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
+mod input_widget;
+mod list_widget;
 
-fn main() -> ! {
-    let mut buf = [0_u8; 1024];
-    let mut stream = TcpStream::connect("192.168.178.170:1234").unwrap();
+use std::{
+    net::TcpStream,
+    time::Duration,
+};
 
-    let mut idx = 0_u32;
-    loop {
-        let n = match stream.read(&mut buf) {
-            Ok(n) => n,
-            Err(_) => continue,
-        };
-        print!("{}", str::from_utf8(&buf[..n]).unwrap());
+use color_eyre::Result;
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers, poll},
+    layout::{Constraint, Layout},
+    DefaultTerminal,
+    Frame,
+};
+use smol::{
+    future::FutureExt, io::AsyncWriteExt, stream::StreamExt, Async, Timer
+};
 
-        idx += 1;
-        if idx == 100 {
-            println!("===================================================================>");
-            let _ = stream.write(b"$FTS,12a,c3,\n");
+
+use input_widget::InputWidget;
+use list_widget::ListWidgets;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let app_result = App::new().run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+
+#[derive(Debug)]
+struct App {
+    input_widget: InputWidget,
+    widgets: ListWidgets<300>,
+
+    should_exit: bool,
+    input: String,
+}
+
+impl App {
+    /// The duration between each tick.
+    const TICK_RATE: Duration = Duration::from_millis(20);
+
+    /// Create a new instance of the app.
+    fn new() -> Self {
+        Self {
+            input_widget: InputWidget::new(),
+            widgets: ListWidgets::new(),
+            should_exit: false,
+            input: String::new(),
         }
     }
 
-} // the stream is closed here
+    /// Run the app until the user exits.
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        smol::block_on(async {
+            let mut stream = Async::<TcpStream>::connect(([192, 168, 178, 170], 1234)).await?;
+
+            while !self.should_exit {
+                FutureExt::or(
+                    async {
+                        let _ = smol::io::copy(&stream, &mut self.widgets).await;
+                    },
+                    async {
+                        Timer::interval(Self::TICK_RATE).next().await;
+                    },
+                )
+                .await;
+
+                let _ = self.handle_events();
+                if self.input.len() > 0 {
+                    self.widgets.cmd().add_item(self.input.clone());
+                    self.input.push('\n');
+                    stream.write_all(self.input.as_bytes()).await?;
+                    self.input.clear();
+                }
+                terminal.draw(|frame| self.render(frame))?;
+            }
+            Ok(())
+        })
+    }
+
+    /// Handle events from the terminal.
+    fn handle_events(&mut self) -> Result<()> {
+        if poll(Duration::from_micros(1))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    self.should_exit = false;
+                    match key.code {
+                        KeyCode::Enter => self.input = self.input_widget.submit_message(),
+                        KeyCode::Char('c') => if key.modifiers == KeyModifiers::CONTROL {
+                            self.should_exit = true;
+                        } else {
+                            self.input_widget.handle_key_input(key.code);
+                        }
+                        _ => self.input_widget.handle_key_input(key.code),
+                    };
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn render(&mut self, frame: &mut Frame) {
+        let outer_layout = Layout::horizontal([
+            Constraint::Length(40), 
+            Constraint::Fill(1)]
+        ).split(frame.area());
+        let right_layout = Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Length(3)
+        ]).split(outer_layout[1]);
+
+        let can_rf_area = outer_layout[0];
+        let command_area = right_layout[0];
+        let input_area = right_layout[1];
+
+        self.input_widget.render(frame, &input_area);
+        self.widgets.render(frame, &can_rf_area, &command_area);
+    }
+}
